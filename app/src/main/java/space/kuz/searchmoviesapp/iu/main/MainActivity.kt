4ad6a66653344594866da4934b7f58ef34e9db2c
Ferpasha <pavel.kuzneczov.96@gmail.com>
2021-11-04 14:23:08 +0500

    Добавление структуры пакетов

--- app/src/main/java/space/kuz/searchmoviesapp/iu/main/MainActivity.kt
+++ app/src/main/java/space/kuz/searchmoviesapp/iu/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package space.kuz.searchmoviesapp
+package space.kuz.searchmoviesapp.iu.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import space.kuz.searchmoviesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
