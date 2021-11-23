e7d4f741f4c3754dad2ada5e0bb47698858ee787
Ferpasha <pavel.kuzneczov.96@gmail.com>
2021-11-23 20:41:36 +0500

    Запись во внутреннюю память и чтение (удаление записей при разрушении сервиса)

--- app/src/main/java/space/kuz/searchmoviesapp/util/mvp/MyService.kt
+++ app/src/main/java/space/kuz/searchmoviesapp/util/mvp/MyService.kt
@@ -8,10 +8,30 @@ import android.util.Log
 import android.widget.Toast
 import space.kuz.searchmoviesapp.iu.main.EVENT
 import space.kuz.searchmoviesapp.iu.main.MainActivity
+import java.io.*
+import java.lang.StringBuilder
 import java.security.Provider
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
+import android.R
+
+import android.os.Environment
+import androidx.core.content.PackageManagerCompat
+import java.lang.Exception
+import androidx.core.content.PackageManagerCompat.LOG_TAG
+import androidx.core.content.PackageManagerCompat.LOG_TAG
+import android.system.Os.socket
+
+
+
+
+
+
+
+
+
+
 
 class MyService: Service() {
     private val   TAG = "@@@"
@@ -32,7 +52,50 @@ class MyService: Service() {
        val  timeFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
         var time = timeFormat.format(currentDate)
         Log.d(TAG, "OnStartCommand ${intent?.extras?.getString(EVENT)} ${time} " )
+        writeFile("redic.txt","OnStartCommand ${intent?.extras?.getString(EVENT)} ${time} \n" ,MODE_APPEND)
+        readFile("redic.txt")
+        //Log.d(TAG,  readFile("redic.txt").toString() )
         return super.onStartCommand(intent, flags, startId)
+    }
+    fun writeFile(FileName:String, text:String, mode: Int) {
+        try {
+
+            // отрываем поток для записи
+            val bw = BufferedWriter(
+                OutputStreamWriter(
+                    openFileOutput(FileName,mode)
+                )
+            )
+            // пишем данные
+            bw.write(text)
+            // закрываем поток
+            bw.close()
+        //    Log.d("@@@", "Файл записан")
+
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+    fun readFile(FILENAME:String) {
+        try {
+            // открываем поток для чтения
+            val br = BufferedReader(
+                InputStreamReader(
+                    openFileInput(FILENAME)
+                )
+            )
+            var str: String? = ""
+            // читаем содержимое
+            while (br.readLine().also { str = it } != null) {
+                Log.d("@@@@", str!!)
+            }
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
     }
 
     override fun onUnbind(intent: Intent?): Boolean {
@@ -41,6 +104,7 @@ class MyService: Service() {
     }
 
     override fun onDestroy() {
+        writeFile("redic.txt","nulll" , MODE_PRIVATE)
         super.onDestroy()
         Log.d(TAG, "OnDestroy")
     }
